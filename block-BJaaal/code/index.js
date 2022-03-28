c6bd3b2a9a56b01cc427568b18c97b81673ba818
SiriPavani <siripavani90@gmail.com>
2022-03-28 11:32:47 +0530

    storing values ass2

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -4,6 +4,8 @@
 - Define a variable named `username` using `const` keyword and assign your name as a value.
 - Define another variable named `bio` and assign a value `Learning to code`. Use `let` keyword to define the variable.
 */
+const username = "Siri";
+let bio = "Learning to code";
 
 let firstName = "Arya";
 let lastName = "Stark";
@@ -12,6 +14,8 @@ let lastName = "Stark";
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
+alert(username + bio);
+alert(firstName + lastName);
 
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
